93444edb93a5a1e16ddce5466957360a3c8ffd56
Sergej Sintschilin <seregpie@gmail.com>
2020-06-26 16:56:38 +0200

    +

--- demo/script.js
+++ demo/script.js
@@ -2,23 +2,43 @@
 
 	new Vue({
 		el: '#app',
-
+		vuetify: new Vuetify(),
 		data: {
 			colorObject: tinycolor.random().toRgb(),
 			durationIndex: 1,
-			durationValues: [0, 1000, 5000, 10000],
+			durationItems: [
+				{
+					label: '0s',
+					value: 0,
+				},
+				{
+					label: '1s',
+					value: 1000,
+				},
+				{
+					label: '5s',
+					value: 5000,
+				},
+			],
 		},
-
 		computed: {
 			animatedColor: function() {
 				return tinycolor(this.animatedColorObject).toHexString();
 			},
-
 			duration: function() {
 				return this.durationValues[this.durationIndex];
 			},
+			durationLabels: function() {
+				return this.durationItems.map(function(item) {
+					return item.label;
+				});
+			},
+			durationValues: function() {
+				return this.durationItems.map(function(item) {
+					return item.value;
+				});
+			},
 		},
-
 		tweened: {
 			animatedColorObject: {
 				get: function() {
--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,39 @@
-import buble from 'rollup-plugin-buble';
-import minify from 'rollup-plugin-babel-minify';
-import path from 'path';
-import resolve from '@seregpie/rollup-plugin-resolve';
+import {terser} from 'rollup-plugin-terser';
+import buble from '@rollup/plugin-buble';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
 
 import {main} from './package.json';
 
+let plugins = [
+	resolve(),
+	replace({'process.env.NODE_ENV': JSON.stringify('production')}),
+	commonjs(),
+	buble({objectAssign: 'Object.assign'}),
+	terser(),
+];
+
+if (process.env.ROLLUP_WATCH) {
+	plugins.push(serve({
+		contentBase: '',
+		open: true,
+	}));
+}
+
+let globals = {
+	'@vue/composition-api': 'vueCompositionApi',
+};
+
 export default {
+	external: Object.keys(globals),
 	input: 'src/index.js',
+	plugins,
 	output: {
 		file: main,
 		format: 'umd',
-		name: path.basename(main, path.extname(main)),
+		name: 'VueTween',
+		globals,
 	},
-	plugins: [
-		resolve(),
-		buble({objectAssign: 'Object.assign'}),
-		minify({comments: false}),
-	],
 };
